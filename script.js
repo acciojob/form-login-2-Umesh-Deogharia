36f1ca1ed4ce6e162962677f1b84fe738bda1c69
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-23 19:50:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let fname = document.getElementById('fname');
+let lname = document.getElementById('lname');
+let phone = document.getElementById('phone');
+let email = document.getElementById('email');
+let btn = document.getElementsByTagName('button')[0];
+
+function info(event) {
+	event.preventDefault();
+	alert(`First Name: ${fname.value}`,
+		 `Last Name: ${lname.value}`,
+		 `Phone Number: ${phone.value}`,
+		 `Email ID: ${email.value}`);
+}
+
+btn.addEventListener('click',info);
+
